978757bb9c93e7abb1b86388c63b107e15d496b5
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2011-11-17 14:12:54 +0000

    Moved js and css to dedicated folders

--- includes/head.php
+++ includes/head.php
@@ -2,15 +2,15 @@
 <title>i4Life WP4 Enhanced Download Service of the Catalogue of Life:
 Darwin Core Archive Export</title>
 <link type="text/css"
-    href="jquery-ui/css/smoothness/jquery-ui-1.8.16.custom.css"
+    href="js/jquery-ui/css/smoothness/jquery-ui-1.8.16.custom.css"
     rel="stylesheet" />
-<script type="text/javascript" src="jquery-ui/js/jquery-1.6.2.min.js"></script>
+<script type="text/javascript" src="js/jquery-ui/js/jquery-1.6.2.min.js"></script>
 <script type="text/javascript"
-    src="jquery-ui/js/jquery-ui-1.8.16.custom.min.js"></script>
-<script type="text/javascript" src="includes/form.js"></script>
-<link href="includes/style.css" rel="stylesheet" type="text/css"
+    src="js/jquery-ui/js/jquery-ui-1.8.16.custom.min.js"></script>
+<script type="text/javascript" src="js/form.js"></script>
+<link href="style/style.css" rel="stylesheet" type="text/css"
     media="all">
 <!--[if lte IE 7]>
-    <link href="includes/style-ie7.css" rel="stylesheet" type="text/css"
+    <link href="style/style-ie7.css" rel="stylesheet" type="text/css"
         media="all">
 <![endif]-->
