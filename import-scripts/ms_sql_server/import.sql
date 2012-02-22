28b2554bc64d0506bfcb82fde1ad1dc2dbf9f196
Ayco Holleman <ayco.holleman@naturalis.nl>
2012-02-22 12:32:02 +0000

    Added README.txt and possibility for table prefix

--- import-scripts/ms_sql_server/import.sql
+++ import-scripts/ms_sql_server/import.sql
@@ -1,32 +1,45 @@
--- Modify as appropriate for your own situation!
+-- Modify as appropriate for your own situation. Enter
+-- the directory containing the darwin core archive files
+-- (taxa.txt, distribution.txt, etc.). The path must end
+-- with a backslash.
 DECLARE @export_dir VARCHAR(100) = 'C:\Users\Ayco Holleman\Downloads\dca01\';
 
+-- Modify if you want the table names to have a prefix:
+DECLARE @table_prefix VARCHAR(16) = '';
 
-TRUNCATE TABLE "Taxon";
 
-TRUNCATE TABLE "Distribution";
 
-TRUNCATE TABLE "Reference";
-
-TRUNCATE TABLE "VernacularName";
+EXEC('TRUNCATE TABLE ' + @table_prefix + 'Taxon');
+EXEC('TRUNCATE TABLE ' + @table_prefix + 'Distribution');
+EXEC('TRUNCATE TABLE ' + @table_prefix + 'Reference');
+EXEC('TRUNCATE TABLE ' + @table_prefix + 'VernacularName');
 
 
 
 DECLARE @bulk_cmd varchar(1000);
 
-SET @bulk_cmd = 'BULK INSERT Taxon
+SET @bulk_cmd = 'BULK INSERT ' + @table_prefix + 'Taxon
 FROM ''' + @export_dir + 'taxa.txt'' 
 WITH (FIRSTROW = 2, FIELDTERMINATOR = ''' + CHAR(9) + ''', ROWTERMINATOR = ''' + CHAR(10)+ ''')';
 
-SET @bulk_cmd = 'BULK INSERT Distribution
+EXEC(@bulk_cmd);
+
+
+SET @bulk_cmd = 'BULK INSERT ' + @table_prefix + 'Distribution
 FROM ''' + @export_dir + 'distribution.txt'' 
 WITH (FIRSTROW = 2, FIELDTERMINATOR = ''' + CHAR(9) + ''', ROWTERMINATOR = ''' + CHAR(10)+ ''')';
 
-SET @bulk_cmd = 'BULK INSERT VernacularName
+EXEC(@bulk_cmd);
+
+
+SET @bulk_cmd = 'BULK INSERT ' + @table_prefix + 'VernacularName
 FROM ''' + @export_dir + 'vernacular.txt'' 
 WITH (FIRSTROW = 2, FIELDTERMINATOR = ''' + CHAR(9) + ''', ROWTERMINATOR = ''' + CHAR(10)+ ''')';
 
-SET @bulk_cmd = 'BULK INSERT Reference
+EXEC(@bulk_cmd);
+
+
+SET @bulk_cmd = 'BULK INSERT ' + @table_prefix + 'Reference
 FROM ''' + @export_dir + 'reference.txt'' 
 WITH (FIRSTROW = 2, FIELDTERMINATOR = ''' + CHAR(9) + ''', ROWTERMINATOR = ''' + CHAR(10)+ ''')';
 
