f1fbd8d74152b9ded74aa4493e04fdefa752963f
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2019-03-25 10:35:23 +0100

    from 10000 start cli

--- includes/export.php
+++ includes/export.php
@@ -22,7 +22,7 @@ if (!$dcaExporter->archiveExists()) {
     $dcaExporter->createMetaXml();
     echo 'Writing data to text files...<br>';
     // If the total exceeds 100.000 records, offload to cli
-    if ($total > 1) {
+    if ($total > 10000) {
         passthru($dcaExporter->getCliCommand());
     // Webserver should be able to handle smaller exports
     } else {
